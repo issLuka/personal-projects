af98645f34365bde840e72860791edb88fc3b058
issLuka <loofaunce@gmail.com>
2025-06-21 00:07:16 +0900

    added kana library

--- SoundoutActivityAutomation/PDFEditing.py
+++ SoundoutActivityAutomation/PDFEditing.py
@@ -1,30 +1,32 @@
 #This is a program to automate creating lined a sound out activity I use for teaching
 #Louis Faunce - made 06/20/2025
 import random
+from e2k import P2K #importing e2k phoneme to kana converter
 
+#section for basic variables
+p2k = P2K() #initializing the phoneme to kana converter
 soundOutList = [] #sets up list for words
 
 with open("SoundOutInput.txt", "r") as file: #reads file and puts to list, removing whitespace. "r" is for read only
     for line in file:
         soundOutList.append(line.strip().split("\t")) #formats the words into the list (use * when printing or writing to new file to remove [""]
 
-randomizeList = soundOutList.copy() #sets up list for randomized words
+randomizeList = soundOutList.copy() #sets up list for randomized words copying og list
 
-def randomSpelling():
-    #randomizeList = soundOutList.copy()
+def randomSpelling(): #self explanatory function to randomize the words in the list
     for i in range(len(randomizeList)): #loops through each word in the list and randomizes
         randomizeList[i]= ''.join(random.sample(*randomizeList[i],len(*randomizeList[i]))) 
     return randomizeList #returns the randomized list
 
-def printTests():
-    print("Sound Out Activity Words:", *soundOutList) #prints header
+def printTests(): #tests to make sure lists work
+    print("Sound Out Activity Words:", soundOutList) #prints header
     print("Level 1 Words: ", *levelOneWords) #prints level 1 words
     print("Level 2 Words: ", *levelTwoWords) #prints level 2 words
     print("Level 3 Words: ", *levelThreeWords) #prints level 3 words
     print("Level 4 Words: ", *levelFourWords) #prints level 4 words
     print("Level 5 Words: ", *levelFiveWords) #prints level 5 words
 
-def katakanaize():
+def katakanaize(): #turn og list to kana
 
     return 0 
 
