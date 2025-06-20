a89f5d43133c4a3b00a892176318f927ab21c586
issLuka <loofaunce@gmail.com>
2025-06-20 23:36:24 +0900

    added randomizer for words and each word

--- SoundoutActivityAutomation/PDFEditing.py
+++ SoundoutActivityAutomation/PDFEditing.py
@@ -8,18 +8,36 @@ with open("SoundOutInput.txt", "r") as file: #reads file and puts to list, remov
     for line in file:
         soundOutList.append(line.strip().split("\t")) #formats the words into the list (use * when printing or writing to new file to remove [""]
 
-#function class to run randomizing
+
 def randomSpelling():
-    print(''.join(random.sample(*soundOutList[1],len(*soundOutList[1])))) #edit to use loop and edit each string within the list
-    return 0
+    for i in range(len(soundOutList)): #loops through each word in the list and randomizes
+        soundOutList[i]= ''.join(random.sample(*soundOutList[i],len(*soundOutList[i]))) 
+
+
+randomSpelling()
+
+levelOneWords = soundOutList[0:4] #first four words, level 1 difficulty, followed by setting up lists for each level
+levelTwoWords = soundOutList[5:9] 
+levelThreeWords = soundOutList[10:14] 
+levelFourWords = soundOutList[15:19] 
+levelFiveWords = soundOutList[20:22] 
+
+
+
 
 #function for making words into katakana
 def katakanaize():
 
     return 0 
 
-#loop to randomize every word in level output to levelXwordXjumbled or some, format with " " between each letter.
 
-#loop to make version of each word katakana - output to  or something
 
 #format for output to be sent to new file will be "kana ・ jumbled word"
+
+def printTests():
+    print("Sound Out Activity Words:", *soundOutList) #prints header
+    print("Level 1 Words: ", *levelOneWords) #prints level 1 words
+    print("Level 2 Words: ", *levelTwoWords) #prints level 2 words
+    print("Level 3 Words: ", *levelThreeWords) #prints level 3 words
+    print("Level 4 Words: ", *levelFourWords) #prints level 4 words
+    print("Level 5 Words: ", *levelFiveWords) #prints level 5 words
