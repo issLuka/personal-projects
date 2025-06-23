4dd02986bfdefa6d3ef9b7d90fac7681b460c5a0
issLuka <loofaunce@gmail.com>
2025-06-24 00:09:37 +0900

    edited some testing, trying to get the randomize to work properly

--- testing/stackoverflowbullshit.py
+++ testing/stackoverflowbullshit.py
@@ -8,38 +8,23 @@ with open("SoundOutInput.txt", "r") as file: #reads file and puts to list, remov
 
 randomizeList = soundOutList.copy() #sets up list for randomized words copying og list
 
-def randomSpelling(): #self explanatory function to randomize the words in the list
+def randomSpelling(words: list): #self explanatory function to randomize the words in the list
+    listCopy = words.copy()
+    for i in range(len(listCopy)):
+        listCopy[i] = ''.join(random.sample(*listCopy[i],len(*listCopy[i]))) #randomizes each word in the list
+        if listCopy[i] != words[i]:
+            words[i] = listCopy[i] #if the randomized word is the same as the original, it will not change it
+            print(words[i]) #prints the word to console for testing
 
-    for i in range(len(randomizeList)): #loops through each word in the list and randomizes
-        randomizeList[i] = ''.join(random.sample(*randomizeList[i],len(*randomizeList[i])))
-
-    return randomizeList #returns the randomized list
-
-randomSpelling()
+    
+   
+randomSpelling(randomizeList) #calls the randomSpelling function to randomize the words
 
 levelOneWords = randomizeList[0:4] #first four randomized words, level 1 difficulty, followed by setting up lists for each level
 levelTwoWords = randomizeList[5:9] 
 levelThreeWords = randomizeList[10:14] 
 levelFourWords = randomizeList[15:19] 
 levelFiveWords = randomizeList[20:22] 
-
-def randomSpellCheck():
-    spellCheckBool = False 
-    while spellCheckBool == False: #loops through each word in the list
-        if levelOneWords[0:4] == soundOutList[0:4]: 
-          randomizeList[0:4] = ''.join(random.sample(*randomizeList[0:4],len(*randomizeList[0:4])))
-        elif levelTwoWords[5:9] == soundOutList[5:9]: 
-            randomizeList[5:9] = ''.join(random.sample(*randomizeList[5:9],len(*randomizeList[5:9])))
-        elif levelThreeWords[10:14] == soundOutList[10:14]: 
-            randomizeList[10:14] = ''.join(random.sample(*randomizeList[10:14],len(*randomizeList[10:14])))
-        elif levelFourWords[15:19] == soundOutList[15:19]: 
-            randomizeList[15:19] = ''.join(random.sample(*randomizeList[15:19],len(*randomizeList[15:19])))
-        elif levelFiveWords[20:22] == soundOutList[20:22]: 
-            randomizeList[20:22] = ''.join(random.sample(*randomizeList[20:22],len(*randomizeList[20:22])))
-        else:
-            spellCheckBool = True
-
-randomSpellCheck()
 
 def addSpaceToWords(): #will add words to each level
     for i in range(len(levelOneWords)):
@@ -55,12 +40,8 @@ def addSpaceToWords(): #will add words to each level
 
 addSpaceToWords()
 
-levelOneWordOne = " ".join(levelOneWords[0])  # Joins the first word of level one with spaces
-levelTwoWordOne = " ".join(levelTwoWords[0])  # Joins the first word of level two with spaces
-levelThreeWordOne = " ".join(levelThreeWords[0])  # Joins the first word of level three with spaces
-levelFourWordOne = " ".join(levelFourWords[0])  # Joins the first word of level four with spaces
-levelFiveWordOne = " ".join(levelFiveWords[0])  # Joins the first word of level five with spaces
+
 
 
 print("Original List:", *soundOutList)
-print("Randomized List:", *levelOneWords, "\n", *levelTwoWords, "\n", *levelThreeWords, "\n", *levelFourWords, "\n", *levelFiveWords)
+#print("Randomized List:\n", *levelOneWords, "\n", *levelTwoWords, "\n", *levelThreeWords, "\n", *levelFourWords, "\n", *levelFiveWords)
